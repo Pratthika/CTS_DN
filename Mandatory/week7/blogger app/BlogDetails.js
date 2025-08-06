40d5059bd157c5dc37cc8a85cdfd58d6fc464cfd
Pratthika <119473299+Pratthika@users.noreply.github.com>
2025-08-06 11:10:02 +0530

    Create BlogDetails.js

---
+++ Mandatory/week7/blogger app/BlogDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BlogDetails = () => {
+  return (
+    <div>
+      <h2>📝 Blog Details</h2>
+      <p>Title: Understanding JSX</p>
+      <p>Author: Sophie Alpert</p>
+      <p>Published: August 2023</p>
+    </div>
+  );
+};
+
+export default BlogDetails;
