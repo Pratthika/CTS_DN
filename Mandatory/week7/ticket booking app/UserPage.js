1e340a5773e1e757d40328647ee27beafaf280d9
Pratthika <119473299+Pratthika@users.noreply.github.com>
2025-08-06 11:08:13 +0530

    Create UserPage.js

---
+++ Mandatory/week7/ticket booking app/UserPage.js
@@ -0,0 +1,14 @@
+// src/components/UserPage.js
+import React from 'react';
+
+const UserPage = () => {
+  return (
+    <div>
+      <h2>Welcome Back, User!</h2>
+      <p>You can now book your flight tickets.</p>
+      <button>Book Flight</button>
+    </div>
+  );
+};
+
+export default UserPage;
