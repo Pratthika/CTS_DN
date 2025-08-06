31226a91c2064deb3e93d691698465cf59186683
Pratthika <119473299+Pratthika@users.noreply.github.com>
2025-08-06 11:10:21 +0530

    Create CourseDetails.js

---
+++ Mandatory/week7/blogger app/CourseDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CourseDetails = () => {
+  return (
+    <div>
+      <h2>🎓 Course Details</h2>
+      <ul>
+        <li>Course: Modern React Development</li>
+        <li>Instructor: Kent C. Dodds</li>
+        <li>Duration: 10 Weeks</li>
+      </ul>
+    </div>
+  );
+};
+
+export default CourseDetails;
