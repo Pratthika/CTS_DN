437445128de917105dbfbc1fb3acd8b475402ff2
Pratthika <119473299+Pratthika@users.noreply.github.com>
2025-08-06 11:09:41 +0530

    Create BookDetails.js

---
+++ Mandatory/week7/blogger app/BookDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const BookDetails = () => {
+  return (
+    <div>
+      <h2>📚 Book Details</h2>
+      <ul>
+        <li>Name: React for Beginners</li>
+        <li>Author: Dan Abramov</li>
+        <li>Pages: 280</li>
+      </ul>
+    </div>
+  );
+};
+
+export default BookDetails;
